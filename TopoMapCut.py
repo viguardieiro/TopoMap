b9d9f9014614c2be9eddecc0982ca2e81e956074
Vitoria Guardieiro <vitoriaguardieiro@gmail.com>
2024-02-27 18:08:46 -0500

    Move TopoMapCut to a different file

--- TopoMapCut.py
+++ TopoMapCut.py
@@ -14,7 +14,9 @@ import umap
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
-class TopoMap():
+from TopoMap import TopoMap
+
+class TopoMapCut(TopoMap):
     def __init__(self, points:np.ndarray) -> None:
         self.points = points
         self.n = len(points)
@@ -24,60 +26,93 @@ class TopoMap():
 
         self.projections = np.zeros(shape=(self.n, 2), dtype=np.float32)
         self.components = DisjointSet(list(range(self.n)))
+        self.subsets = None
+        self.points_component = None
+        self.components_points = None
 
-    def get_mst(self):
-        dists = squareform(pdist(self.points))
-        self.mst = minimum_spanning_tree(dists).toarray()
-        return self.mst
+        self.proj_subsets = np.zeros(shape=(self.n, 2), dtype=np.float32)
+
+        self.last_processed_edge = -1
     
-    def get_sorted_edges(self):
-        G = nx.from_numpy_array(self.mst)
-        self.sorted_edges = sorted(G.edges(data=True), 
-                                   key=lambda edge: edge[2].get('weight', 1))
-        return self.sorted_edges
+    def get_component_of_points(self):
+        if self.subsets is None:
+            self.subsets = self.components.subsets()
+        self.points_component = np.zeros(self.n)
+
+        for i in range(self.n):
+            for j in range(len(self.subsets)):
+                if i in self.subsets[j]:
+                    self.points_component[i] = j
+
+        return self.points_component
     
-    def rotate_component(self, 
-                         component_points:np.ndarray, 
-                         ref_point:np.ndarray, 
-                         direction='top') -> np.ndarray:
-        
-        if len(component_points) == 1:
-            return component_points, [0,0]
-        
-        hull = get_hull(component_points)
+    def get_points_of_components(self):
+        if self.subsets is None:
+            self.subsets = self.components.subsets()
+        self.components_points = []
 
-        closest_edge, edge_i = closest_edge_point(hull, ref_point)
-        
-        t = Transform()
-        t.cos, t.sin = find_angle(closest_edge)
-        component_points = t.rotate(component_points)
+        for j in range(len(self.subsets)):
+            self.components_points.append([])
+            for i in self.subsets[j]:
+                self.components_points[-1].append(self.points[i,:])
+            self.components_points[-1] = np.array(self.components_points[-1])
 
-        component_points = fix_rotation(component_points[edge_i], 
-                                        component_points, 
-                                        direction=direction)
-
-        return component_points, edge_i
+        return self.components_points
     
-    def translate_component(self, 
-                            component_points:np.ndarray, 
-                            edge_i:np.ndarray,
-                            to_point:list) -> np.ndarray:
-        
-        if component_points[edge_i[0], 0] <= component_points[edge_i[1], 0]:
-            t = Transform(x = to_point[0]-component_points[edge_i[0], 0], 
-                          y = to_point[1]-component_points[edge_i[0], 1])
-            component_points = t.translate(component_points)
+    def get_components(self, max_components=-1, 
+                       min_dist=-1):
+        for i in range(min(self.n, len(self.sorted_edges))):
+            # Get points from the edge
+            i_a, i_b = self.sorted_edges[i][0], self.sorted_edges[i][1]
 
-        else:
-            t = Transform(x = to_point[0]-component_points[edge_i[1], 0], 
-                          y = to_point[1]-component_points[edge_i[1], 1])
-            component_points = t.translate(component_points)
+            # Distance between points
+            d = self.sorted_edges[i][2]['weight']
 
-        return component_points
+            if min_dist!=-1 and d >= min_dist:
+                break
+            if max_components!=-1 and len(self.components.subsets()) <= max_components:
+                break
+            
+            # Merge components 
+            self.components.merge(i_a, i_b)
+
+        self.last_processed_edge = i
+        self.subsets = self.components.subsets()
+        self.points_component = self.get_component_of_points()
+        self.components_points = self.get_points_of_components()
+
+        return self.components
     
-    def run_iter(self, iter=5):
-        fig, ax = plt.subplots(1,3, figsize=(12,4))
-        for i in range(iter):
+    def project_components(self, proj_method='tsne', perplexity=30):
+        self.proj_subsets = []
+
+        if self.subsets is None:
+            self.subsets = self.components.subsets()
+
+        for j in range(len(self.subsets)):
+            self.proj_subsets.append([])
+
+            if proj_method=='tsne' and len(self.subsets[j]) > perplexity:
+                proj = TSNE(n_components=2, perplexity=perplexity)
+                self.proj_subsets[-1] = proj.fit_transform(self.components_points[j])
+
+            elif proj_method=='umap':
+                proj = umap.UMAP(n_components=2)
+                self.proj_subsets[-1] = proj.fit_transform(self.components_points[j])
+
+            elif len(self.subsets[j]) >= 2 or (proj_method=='pca' and len(self.subsets[j]) >= 2):
+                proj = PCA(n_components=2)
+                self.proj_subsets[-1] = proj.fit_transform(self.components_points[j])
+                
+            else:
+                self.proj_subsets[-1] = np.array([[0,0]])
+
+            self.projections[list(self.subsets[j]), :] = self.proj_subsets[-1]
+
+        return self.proj_subsets
+    
+    def join_components(self):
+        for i in range(self.last_processed_edge, min(self.n, len(self.sorted_edges))):
             # Get points from the edge
             i_a, i_b = self.sorted_edges[i][0], self.sorted_edges[i][1]
             p_a, p_b = self.projections[i_a,:], self.projections[i_b,:]
@@ -90,7 +125,39 @@ class TopoMap():
             proj_c_a, proj_c_b = self.projections[list(c_a)], self.projections[list(c_b)]
             i_a_comp, i_b_comp = list(c_a).index(i_a), list(c_b).index(i_b)
 
-            if i==iter-1:
+            # Rotate the first to be the topmost
+            proj_c_a, edge_t = self.rotate_component(proj_c_a, p_a, direction='top')
+            proj_c_b, edge_b = self.rotate_component(proj_c_b, p_b, direction='bottom')
+
+            # Rotate the second to be the bottomost
+            proj_c_a = self.translate_component(proj_c_a, edge_t, to_point=[0,0])
+            proj_c_b = self.translate_component(proj_c_b, edge_b, to_point=[0,d])
+
+            # Merge components 
+            self.components.merge(i_a, i_b)
+
+            self.projections[list(c_a), :] = proj_c_a
+            self.projections[list(c_b), :] = proj_c_b
+
+        return self.projections
+
+    def run_iter(self, iter=1):
+        fig, ax = plt.subplots(1,3, figsize=(12,4))
+
+        for i in range(self.last_processed_edge, self.last_processed_edge+iter):
+            # Get points from the edge
+            i_a, i_b = self.sorted_edges[i][0], self.sorted_edges[i][1]
+            p_a, p_b = self.projections[i_a,:], self.projections[i_b,:]
+
+            # Distance between points
+            d = self.sorted_edges[i][2]['weight']
+            
+            # Get components the points belong to
+            c_a, c_b = self.components.subset(i_a), self.components.subset(i_b)
+            proj_c_a, proj_c_b = self.projections[list(c_a)], self.projections[list(c_b)]
+            i_a_comp, i_b_comp = list(c_a).index(i_a), list(c_b).index(i_b)
+
+            if i==self.last_processed_edge+iter-1:
                 print(f'Number of points in A: {len(proj_c_a)}')
                 print(f'Number of points in B: {len(proj_c_b)}')
                 print(f'Distance: {d}')
@@ -112,7 +179,7 @@ class TopoMap():
             # Rotate the second to be the bottomost
             proj_c_b, edge_b = self.rotate_component(proj_c_b, p_b, direction='bottom')
 
-            if i==iter-1:
+            if i==self.last_processed_edge+iter-1:
                 ax[1].scatter(self.projections[:,0], self.projections[:,1], s=5, c='black', 
                               alpha=0.1, linewidths=0)
                 
@@ -135,7 +202,7 @@ class TopoMap():
             proj_c_a = self.translate_component(proj_c_a, edge_t, to_point=[0,0])
             proj_c_b = self.translate_component(proj_c_b, edge_b, to_point=[0,d])
 
-            if i==iter-1:
+            if i==self.last_processed_edge+iter-1:
                 ax[2].scatter(self.projections[:,0], self.projections[:,1], s=5, c='black', 
                               alpha=0.1, linewidths=0)
                 
@@ -162,34 +229,4 @@ class TopoMap():
             self.projections[list(c_b), :] = proj_c_b
 
         return self.projections, self.components
-    
-    def run(self):
-        for i in range(min(self.n, len(self.sorted_edges))):
-            # Get points from the edge
-            i_a, i_b = self.sorted_edges[i][0], self.sorted_edges[i][1]
-            p_a, p_b = self.projections[i_a,:], self.projections[i_b,:]
-
-            # Distance between points
-            d = self.sorted_edges[i][2]['weight']
-            
-            # Get components the points belong to
-            c_a, c_b = self.components.subset(i_a), self.components.subset(i_b)
-            proj_c_a, proj_c_b = self.projections[list(c_a)], self.projections[list(c_b)]
-
-            # Rotate the first to be the topmost
-            proj_c_a, edge_t = self.rotate_component(proj_c_a, p_a, direction='top')
-            # Rotate the second to be the bottomost
-            proj_c_b, edge_b = self.rotate_component(proj_c_b, p_b, direction='bottom')
-
-            # Translate components
-            proj_c_a = self.translate_component(proj_c_a, edge_t, to_point=[0,0])
-            proj_c_b = self.translate_component(proj_c_b, edge_b, to_point=[0,d])
-
-            # Merge components 
-            self.components.merge(i_a, i_b)
-
-            self.projections[list(c_a), :] = proj_c_a
-            self.projections[list(c_b), :] = proj_c_b
-
-        return self.projections
     
